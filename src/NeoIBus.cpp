5611ff4ae9d945979775e97863968a03db42ad69
AlexBeem <71223577+AlexBeem@users.noreply.github.com>
2021-11-23 18:49:27 +0300

    Create NeoIBus.cpp

---
+++ src/NeoIBus.cpp
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+#include "NeoIBus.h"
+#include "NeoICSerial.h"
+SIBus IBus;
+void SIBus::begin(NeoICSerial& serial)
+{
+  begin((Stream&)serial);
+}
+
+void SIBus::begin(Stream& stream)
+{
+  this->stream = &stream;
+  this->state = DISCARD;
+  this->last = millis();
+  this->ptr = 0;
+  this->len = 0;
+  this->chksum = 0;
+  this->lchksum = 0;
+}
+
+void SIBus::loop(void)
+{
+  while (stream->available() > 0)
+  {
+    uint32_t now = millis();
+    if (now - last >= PROTOCOL_TIMEGAP)
+    {
+      state = GET_LENGTH;
+    }
+    last = now;
+    
+    uint8_t v = stream->read();
+    switch (state)
+    {
+      case GET_LENGTH:
+        if (v <= PROTOCOL_LENGTH)
+        {
+          ptr = 0;
+          len = v - PROTOCOL_OVERHEAD;
+          chksum = 0xFFFF - v;
+          state = GET_DATA;
+        }
+        else
+        {
+          state = DISCARD;
+        }
+        break;
+
+      case GET_DATA:
+        buffer[ptr++] = v;
+        chksum -= v;
+        if (ptr == len)
+        {
+          state = GET_CHKSUML;
+        }
+        break;
+        
+      case GET_CHKSUML:
+        lchksum = v;
+        state = GET_CHKSUMH;
+        break;
+
+      case GET_CHKSUMH:
+        // Validate checksum
+      if (chksum == static_cast<uint16_t>(v << 8) + lchksum)  // было if (chksum == (v << 8) + lchksum)
+        {
+          // Execute command - we only know command 0x40
+          switch (buffer[0])
+          {
+            case PROTOCOL_COMMAND40:
+              // Valid - extract channel data
+              for (uint8_t i = 1; i < PROTOCOL_CHANNELS * 2 + 1; i += 2)
+              {
+                channel[i / 2] = buffer[i] | (buffer[i + 1] << 8);
+              }
+              break;
+
+            default:
+              break;
+          }
+        }
+        state = DISCARD;
+        break;
+
+      case DISCARD:
+      default:
+        break;
+    }
+  }
+}
+
+uint16_t SIBus::readChannel(uint8_t channelNr)
+{
+  if (channelNr < PROTOCOL_CHANNELS)
+  {
+    return channel[channelNr];
+  }
+  else
+  {
+    return 0;
+  }
+}
